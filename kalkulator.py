6eef5199c39b8f624dc9903a761afa3af027fa4a
Salmaslbla <salmasalsabila1709@gmail.com>
2022-11-04 10:17:45 +0700

    penjumlahan

--- kalkulator.py
+++ kalkulator.py
@@ -7,3 +7,11 @@ print('4. Pembagian')
 print('='*10)
 
 option = input('Silahkan pilih operasi 1/2/3/4')
+bil_1 = eval(input('Masukkan bilangan pertama: '))
+bil_2 = eval(input('Masukkan bilangan kedua: '))
+
+print('=' *10)
+
+if option == '1':
+    hasil = bil_1 + bil_2
+    print(f'Hasil operasi dari {bil_1} + {bil_2} = {hasil}')
